b079a9b9dbd78cc666b08515b7f2a8ace7d4c5b5
s150923 <s150923@berufskolleg-borken.de>
2019-03-20 08:42:54 +0100

    ...

--- server.js
+++ server.js
@@ -1,58 +1,56 @@
-// Das ist ein einzeiliger Kommentar.
-// Das ist ein einzeiliger Kommentar.
-
-/**
- * das
- * ist
- * ein
- * mehrzeiliger 
- * Kommentar
- */
-
-const bodyparser = require('body-parser')
-
 const express = require('express')
-
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
 
- app.set('view engine', 'ejs')
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server lauscht auf Port %s', server.address().port)    
+})
 
- app.use(bodyparser.urlencoded({extended: true}))
+app.get('/',(req, res, next) => {   
 
- app.use(express.static('public'))
+    let idKunde = req.cookies['istAngemeldetAls']
+    
+    if(idKunde){
+        console.log("Kunde ist angemeldet als " + idKunde)
+        res.render('index.ejs', {                              
+        })
+    }else{
+        res.render('login.ejs', {                    
+        })    
+    }
+})
 
- const server = app.listen(process.env.PORT || 3000, () => {
-     console.log('Der Server ist erfolgreich getartet auf Port %s', server.address().port)
- })
-
- app.get('/',(req,res, next) => {
-     res.render('index.ejs', {        
-     })
- })
-
- app.get('/login',(req,res, next) => {
-    res.render('login.ejs', {        
+app.get('/login',(req, res, next) => {         
+    res.cookie('istAngemeldetAls', '')       
+    res.render('login.ejs', {                    
     })
 })
 
-app.post('/',(req, res, next) => {
+app.get('/login',(req, res, next) => {   
+    res.render('login.ejs', {                    
+    })
+})
+
+app.post('/',(req, res, next) => {   
 
     const idKunde = req.body.idKunde
     const kennwort = req.body.kennwort
 
-    if(idKunde === "4711" && kennwort === "123"){
+    if(idKunde ==="4711" && kennwort ==="123"){
         console.log("Der Cookie wird gesetzt")
         res.cookie('istAngemeldetAls','idKunde')
-        res.render('index.ejs', {  
+        res.render('index.ejs', {                    
         })
     }else{
         console.log("Der Cookie wird gelöscht")
         res.cookie('istAngemeldetAls','')
-        res.render('login.ejs', {  
+        res.render('login.ejs', {                    
         })
     }
-
-   res.render('login.ejs', {  
-   })
 })
- 
+
