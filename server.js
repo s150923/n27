0c9117b6bc986099d7e5c91aeaf9daeb4b34dac2
s150923 <s150923@berufskolleg-borken.de>
2019-10-09 12:14:33 +0200

    ...

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-// Klassendefinition
+// Klassendefinition: Die Klasse ist der Bauplan, der alle relevanten 
+// Eigenschaften enthält.
+// Nach der Deklaration wird mit den reservierten Wort 
+// 'new' ein Objekt der Klasse instanziiert.
 
 class Konto{
     constructor(){
@@ -168,7 +171,15 @@ app.post('/kontoAnlegen',(req, res, next) => {
     
     if(idKunde){
 
+    // Von der Klasse Konto wird ein Objekt namens konto
+    // instanziiert
+
         let konto = new Konto()
+
+        // Nach der Deklaration und der Instanziierung kommt die
+        // Initialisierung. Das heißt, dass konkrete Egenschafts-
+        // werte dem Objekt zugewiesen werden. 
+
         konto.Kontonummer = req.body.kontonummer
         konto.Kontoart = req.body.kontoart
 
